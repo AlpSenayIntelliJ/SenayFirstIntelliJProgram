3e966428185b05c0cbc7fd5751a72e665a2abf2a
BT_3W2_23 <alps2789@nycstudents.net>
2022-09-28 12:22:01 -0400

    updating

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello World! Give me one number to divide by 2: ");
+        System.out.print("Hello World! Give me one number to divide by 2: ");
         int userNum = input.nextInt();
 
         if(userNum%2 != 0){
             System.out.println("This is not an even number so I can't divide it by 2!");
-            System.exit(0);
         }
 
         int finalNum = (userNum/2);
         System.out.println("Your new number is: " + finalNum);
+        System.out.println("I'm a change :)");
     }
 }
