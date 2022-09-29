c6fa183a07ab07245155b8f25e4cc0981c161cee
AlpSenayIntelliJ <alps2789@nycstudents.net>
2022-09-29 11:58:59 -0400

    updating 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 
         if(userNum%2 != 0){
             System.out.println("This is not an even number so I can't divide it by 2!");
+            System.exit(0);
         }
 
         int finalNum = (userNum/2);
