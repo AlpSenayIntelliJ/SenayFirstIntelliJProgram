af246fb26daed18b5de23f9476846356adf20021
BT_3W2_21 <alps2789@nycstudents.net>
2023-02-01 11:20:45 -0500

    updating

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,19 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Hello World! Give me one number to divide by 2: ");
-        int userNum = input.nextInt();
+        ArrayList<String> alp = new ArrayList<String>();
+        alp.add("Sports");
+        alp.add("Spark");
+        alp.add("SHake Shack");
+        alp.add("Mr. FOlwell is the goat!!");
+        System.out.println(alp);
+        System.out.println(alp.size());
+        String replacedStr = alp.set(0, alp.get(3));
+        System.out.println(alp.get(0));
+        System.out.println(replacedStr);
+        alp.set(0, "HELLO!");
+        System.out.println(alp);
 
-        if(userNum%2 != 0){
-            System.out.println("This is not an even number so I can't divide it by 2!");
-            System.exit(0);
-        }
-
-        int finalNum = (userNum/2);
-        System.out.println("Your new number is: " + finalNum);
-        System.out.println("I'm a change :)");
     }
 }
